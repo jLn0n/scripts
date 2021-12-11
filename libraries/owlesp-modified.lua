12c61cce77500301742af36c2528fd2d622c69fc
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-12-11 22:20:27 +0800

    name highlight fixes

--- libraries/owlesp-modified.lua
+++ libraries/owlesp-modified.lua
@@ -114,7 +114,7 @@ function OwlESP:update()
 
 				espBox.Visible = espBoxVisible and visible
 				tracer.Visible = tracerVisible and visible
-				name.Visible = visible
+				name.Visible = espBoxVisible and visible
 			else
 				espBox.Visible = false
 				tracer.Visible = false
