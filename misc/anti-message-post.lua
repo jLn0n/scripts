7fafc8f10715ea334e22046746a0a7f7db771123
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-01-31 22:52:13 +0800

    lack of effort

---
+++ misc/anti-message-post.lua
@@ -0,0 +1,55 @@
+--[[
+	anti-message-post.lua (modified thingy script of https://v3rmillion.net/showthread.php?tid=1150896.)
+	a script that just works like the orig script but with memes
+	idk why did i add memes but ye unfunny and lack of effort meme texts thingys
+--]]
+if not game:IsLoaded() then game.Loaded:Wait() end
+-- services
+local players = game:GetService("Players")
+-- objects
+local player = players.LocalPlayer
+local plrScripts = player.PlayerScripts
+local chatScript = plrScripts:FindFirstChild("ChatScript")
+local chatMain = chatScript:FindFirstChild("ChatMain")
+-- variables
+local oldFunc, oldIndex
+local memes = {
+	"synpase x craked 2073 free no virus",
+	"if u are reading this then u are a skid.",
+	"i feel so bad about skids reading my chat logs",
+	"\n\nnewline\n",
+	"how about u read some memes on the chat logs?",
+	"a chad modifies the messageposted thingy, what happens next is memes",
+	"\"plus ratio\" thingys are cringe ngl",
+	"what do u expect its all memes bleachiñ",
+	"dude im not exploiting because im a chad bruh",
+	"i hope roblox doesnt ban me with this stuff that im saying",
+	"kids be like: block hax0r",
+	"Also try Minecraft!",
+	"Also try Terraria!",
+	"a admin reads chatlogs, what happens is shocking",
+	"bahog bhielat",
+	"ukininayo~!",
+	"\0\n\r"
+}
+-- main
+if player and chatMain then
+	local messagePosted = require(chatMain).MessagePosted
+	local chattedEvent = Instance.new("BindableEvent")
+	chattedEvent.Name = player.Name .. "-ChattedEvent"
+
+	oldFunc = hookfunction(messagePosted.fire, function(...)
+		local self, message = ...
+
+		task.spawn(chattedEvent.Fire, chattedEvent, message)
+		return oldFunc(self, memes[math.random(1, #memes)])
+	end)
+	oldIndex = hookmetamethod(game, "__index", newcclosure(function(...)
+		local self, index = ...
+
+		if self == player and index == "Chatted" then
+			return chattedEvent.Event
+		end
+		return oldIndex(...)
+	end))
+end
