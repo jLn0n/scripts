517dfa3753c845c3c7f6a7bf80306d698e400b08
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-11-20 16:39:12 +0800

    roblox don't u dare to patch this script u fucking
    asshole

---
+++ misc/fe-headless.lua
@@ -0,0 +1,35 @@
+-- services
+local players = game:GetService("Players")
+local starterGui = game:GetService("StarterGui")
+-- objects
+local player = players.LocalPlayer
+local character = player.Character
+local humanoid = character:FindFirstChildWhichIsA("Humanoid")
+local head, torso = character:FindFirstChild("Head"), character:FindFirstChild("Torso")
+local resetBindable = Instance.new("BindableEvent")
+-- variables
+local destroyFunc, resetBindableConnection = character.Destroy, nil
+-- main
+-- initializes the permadeath
+player.Character = nil
+player.Character = character
+task.wait(players.RespawnTime + .05)
+
+humanoid.BreakJointsOnDeath = false
+humanoid:SetStateEnabled(Enum.HumanoidStateType.Dead, false)
+task.defer(destroyFunc, (if humanoid.RigType == Enum.HumanoidRigType.R6 then torso.Neck else head.Neck)) -- destroys the weld of the head first for some magic
+task.defer(destroyFunc, head) -- and we destroy the head
+
+resetBindableConnection = resetBindable.Event:Connect(function()
+	starterGui:SetCore("ResetButtonCallback", true)
+	resetBindableConnection:Disconnect()
+
+	character:Destroy()
+	local daModel = Instance.new("Model")
+	local _daModelHumanoid = Instance.new("Humanoid")
+	_daModelHumanoid.Parent = daModel
+	player.Character = daModel
+
+	task.delay(players.RespawnTime, destroyFunc, daModel)
+end)
+starterGui:SetCore("ResetButtonCallback", resetBindable)
