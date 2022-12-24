730139bb83e134e6f29ced04878fbca89ecb6c22
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-12-24 21:01:01 +0800

    config v4

--- backdoor-executor/bexe-config.lua
+++ backdoor-executor/bexe-config.lua
@@ -1,6 +1,40 @@
--- ALWAYS BACKUP YOUR CONFIG
+-- objects
+local player = game:GetService("Players").LocalPlayer
+
+-- main
 return {
-	["cachedPlaces"] = {
+	["configVer"] = 4, -- don't touch this!
+	-- tweaks
+	["redirectRemote"] = false, -- [BETA] uses a custom remote for server-side execution
+
+	-- customization
+	["autoExec"] = { -- scripts that executes after backdoor is found
+		[[print("backdoor-executor.lua is epic!")]],
+	},
+
+	["remoteFilters"] = { -- remote filters that you don't want to be scanned
+		["AdminRemotes"] = function(remoteObj)
+			local remoteObjPath = remoteObj:GetFullName()
+
+			return string.find(remoteObjPath, "HDAdminClient") or string.find(remoteObjPath, "Basic Admin Essentials")
+		end,
+		["AdonisRemotes"] = function(remoteObj)
+			return (
+				(remoteObj.Parent and remoteObj.Parent:IsA("ReplicatedStorage") and remoteObj:FindFirstChild("__FUNCTION")) or
+				(remoteObj.Name == "__FUNCTION" and remoteObj.Parent:IsA("RemoteEvent") and remoteObj.Parent.Parent:IsA("ReplicatedStorage"))
+			)
+		end,
+		["RobloxReplicatedStorage"] = function(remoteObj)
+			return (if remoteObj.Parent then remoteObj.Parent:IsA("RobloxReplicatedStorage") else nil)
+		end
+	},
+
+	["scriptMacros"] = { -- prefixed as "%macro%" | example %username% -> "Roblox"
+		["username"] = player.Name,
+		["userid"] = player.UserId
+	},
+
+	["cachedPlaces"] = { -- backdoor remote configuration cache thing
 		[5033592164] = {
 			["Path"] = game.PlaceId == 5033592164 and game:GetService("ReplicatedStorage"):GetChildren()[1]:GetFullName(),
 			["Args"] = {"1234567890", "source"}
@@ -10,12 +44,4 @@ return {
 			["Args"] = {"source"}
 		},
 	},
-	["autoExec"] = {
-		[[print("backdoor-executor.lua is epic!")]],
-	},
-	["blacklistSettings"] = {
-		["eventNames"] = {},
-		["eventParentNames"] = {}
-	},
-	["configVer"] = 3 -- don't touch this!
 }
